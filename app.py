03a2ce624b5a446916b6dd59ce28b8b801a33d00
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2022-09-03 13:53:03 -0300

    att

--- app.py
+++ app.py
@@ -1,24 +1,58 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///carros.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cars.sqlite3'
 
 db = SQLAlchemy(app)
 
-class Carros(db.Model):
-    modelo = db.Column('modelo', db.Integer, primary_key=True, autoincrement=True)
-    marca = db.column('marca', db.String(20))
-    valor = db.column('valor', db.Float)
 
-    def __init__(self, marca, valor):
-        self.marca = marca
+class Produto(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(150))
+    valor = db.Column(db.Integer)
+
+    def __init__(self, nome, valor):
+        self.nome = nome
         self.valor = valor
+
 
 @app.route('/')
 def index():
-    carros = Carros.query.all()
-    return render_template('index.html', carros=carros)
+    carros = Produto.query.all()
+    return render_template('index.html', estudantes=carros)
+
+
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        car_add = Produto(request.form['nome'], request.form['valor'])
+        db.session.add(car_add)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add.html')
+
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    car_edit = Produto.query.get(id)
+    if request.method == 'POST':
+        car_edit.nome = request.form['nome']
+        car_edit.valor = request.form['valor']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit.html', estudante=car_edit)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    car_delete = Produto.query.get(id)
+    db.session.delete(car_delete)
+    db.session.commit()
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     db.create_all()
