d3e9f9b26aa70ac4824263559c0e0dcd9e9859db
Daniel Rodrigues <86251321+DanielRodri87@users.noreply.github.com>
2022-09-03 14:26:20 -0300

    atualização main

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///estudantes.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cars.sqlite3'
 
 db = SQLAlchemy(app)
 
@@ -11,10 +11,14 @@ class Produto(db.Model):
     id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(150))
     valor = db.Column(db.Integer)
+    marca = db.Column(db.String(150))
+    ano = db.Column(db.Integer)
 
-    def __init__(self, nome, valor):
+    def __init__(self, nome, valor, marca, ano):
         self.nome = nome
         self.valor = valor
+        self.marca = marca
+        self.ano = ano
 
 
 @app.route('/')
@@ -28,7 +32,7 @@ def index():
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        car_add = Produto(request.form['nome'], request.form['valor'])
+        car_add = Produto(request.form['nome'], request.form['valor'], request.form['marca'], request.form['ano'])
         db.session.add(car_add)
         db.session.commit()
         return redirect(url_for('index'))
@@ -41,6 +45,8 @@ def edit(id):
     if request.method == 'POST':
         car_edit.nome = request.form['nome']
         car_edit.valor = request.form['valor']
+        car_edit.marca = request.form['marca']
+        car_edit.ano = request.form['ano']
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('edit.html', estudante=car_edit)
